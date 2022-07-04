4c625168ca87d94dc4eb7faa5e2e7b2ee9f00b34
lucivaldoqueiroz <lucivaldo.npqueiroz@gmail.com>
2022-07-04 15:21:13 -0300

    projeto pokedex

---
+++ PokedexSwift/PokedexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftApp.swift
+//  PokedexSwift
+//
+//  Created by Lucivaldo Queiroz on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PokedexView()
+        }
+    }
+}
---
+++ PokedexSwift/View/PokeDexView.swift
@@ -0,0 +1,32 @@
+//
+//  PokedexView.swift
+//  PokedexSwift
+//
+//  Created by Lucivaldo Queiroz on 30/06/22.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    @ObservedObject var viewModel = PokemonViewModel()
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(viewModel.pokemon) { pokemon in
+                        PokemonCell(pokemon: pokemon, viewModel: viewModel)
+                    }
+                }
+            }
+            .navigationTitle("Pokedex")
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
